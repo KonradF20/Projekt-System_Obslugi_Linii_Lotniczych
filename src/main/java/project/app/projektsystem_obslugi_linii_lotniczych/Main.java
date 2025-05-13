fc495c225ef9f813a3f1331278bdf4e83756336b
KonradF20 <konradf77@interia.pl>
2025-05-13 21:45:21 +0200

    Drugi commit

--- src/main/java/project/app/projektsystem_obslugi_linii_lotniczych/Main.java
+++ src/main/java/project/app/projektsystem_obslugi_linii_lotniczych/Main.java
@@ -2,22 +2,24 @@ package project.app.projektsystem_obslugi_linii_lotniczych;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class Main extends Application {
+
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("loginPage.fxml")));
+        stage.setTitle("FlyNow");
+        stage.setScene(new Scene(root));
         stage.show();
     }
 
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
 }
