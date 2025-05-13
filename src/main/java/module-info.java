de58b380d645068d193a1187938726ad4b4819a6
KonradF20 <konradf77@interia.pl>
2025-05-13 21:29:38 +0200

    Pierwszy commit
    # Conflicts:
    #	.gitignore

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module project.app.projektsystem_obslugi_linii_lotniczych {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens project.app.projektsystem_obslugi_linii_lotniczych to javafx.fxml;
+    exports project.app.projektsystem_obslugi_linii_lotniczych;
+}
